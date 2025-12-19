caa846f7e205a9ee80a2d2f1fb493a410b59c703
sumanth <sumanthsumanthacharya8@gmail.com>
2025-12-19 20:01:49 +0530

    created object

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//Type Annotation
 var num = 5;
 console.log(num);
+//object 
+var person = {
+    name: "jake",
+    age: 30,
+    salary: 5000
+};
+console.log(person);
--- index.ts
+++ index.ts
@@ -7,3 +7,13 @@ console.log(num);
 
 //object 
 
+const person:{
+    name:string,
+    age:number,
+    salary:number
+}={
+    name:"jake",
+    age:30,
+    salary:5000
+};
+console.log(person);
